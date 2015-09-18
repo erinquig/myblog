81e7b5ae6b74f5143ceeaea4686d128a10fa113d
erinquig <erinquig@gmail.com>
2015-09-17 21:05:09 -0700

    started boxman

--- scripts/erin.js
+++ scripts/erin.js
@@ -1,3 +1,27 @@
 $(function() {
-	console.log("test");
+
+
+	$(document).on('keydown', function(e) {
+		console.log(e.keyCode);
+
+		var boxMan = $('#boxMan');
+
+		if(e.keyCode === 39) {
+						
+			var oldPosition = parseInt(boxMan.css('left'));
+			
+			var newPosition = oldPosition + 10 + 'px';
+
+			boxMan.css('left', newPosition);
+			
+		} else if (e.keyCode === 40) {
+
+		} else if (e.keyCode === 38) {
+
+		} else if (e.keyCode === 37) {
+
+		}
+
+	});
+
 });
